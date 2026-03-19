c29e9a80a24a306eb2acb5a1fd6c050aeab3e5b9
Fernando López <vh343fs@gmail.com>
2026-03-19 11:36:47 -0600

    Update manager model

---
+++ app/domain/manager/model.py
@@ -0,0 +1,28 @@
+from app.shared.base_domain.model import BaseTable
+from app.domain.personal_data.model import DatosSensibles
+from sqlmodel import Field, Relationship
+from app.domain.service.model import Servicio
+
+
+
+class Gerente(BaseTable, table=True):
+    __tablename__ = "gerente"
+
+    datos_sensibles_id: UUID = Field(foreign_key="datos_sensibles.id", unique=True)
+    activo: bool = Field(default=True)
+
+    datos_sensibles: DatosSensibles = Relationship(back_populates="gerente")
+    gerente_servicios: list["GerenteServicio"] = Relationship(back_populates="gerente")
+
+
+class GerenteServicio(BaseTable, table=True):
+    __tablename__ = "gerente_servicio"
+
+    gerente_id: UUID = Field(foreign_key="gerente.id")
+    servicio_id: UUID = Field(foreign_key="servicio.id")
+
+    gerente: Gerente = Relationship(back_populates="gerente_servicios")
+    servicio: Servicio = Relationship(back_populates="gerente_servicios")
+    tickets_ecosistema: list["TicketEcosistema"] = Relationship(
+        back_populates="gerente_servicio"
+    )    
