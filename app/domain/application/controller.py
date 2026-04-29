24ce00587379ee3425eac538d1d25a72b3ecf61a
Ulises <123oiler05@gmail.com>
2026-04-29 16:33:47 -0600

    feat: add oso authorization dependencies to application and service controllers

--- app/domain/application/controller.py
+++ app/domain/application/controller.py
@@ -6,6 +6,8 @@ from app.database import SessionDep
 from app.shared.middleware.auth.applications.auth import CryptoManager
 from app.shared.session.service import SessionService
 from app.config import settings
+from app.shared.authorization.dependencies import require_read, require_write, require_delete  # nuevo
+from app.database.model import Application  # nuevo
 
 
 class ApplicationController(FullCrudApiController):
@@ -15,6 +17,13 @@ class ApplicationController(FullCrudApiController):
     response_schema = ApplicationResponse
     create_schema = ApplicationCreate
     update_schema = ApplicationUpdate
+
+    # nuevo
+    list_dependencies = [require_read(Application)]
+    retrieve_dependencies = [require_read(Application)]
+    create_dependencies = [require_write(Application)]
+    update_dependencies = [require_write(Application)]
+    delete_dependencies = [require_delete(Application)]
 
 
 application_router = ApplicationController().router
--- app/domain/service/controller.py
+++ app/domain/service/controller.py
@@ -1,6 +1,8 @@
 from app.shared.base_domain.controller import FullCrudApiController
 from app.domain.service.schemas import ServiceCreate, ServiceResponse, ServiceUpdate
 from app.domain.service.service import ServiceServiceDep
+from app.shared.authorization.dependencies import require_read, require_write, require_delete  # nuevo
+from app.database.model import Service  # nuevo
 
 
 class ServiceController(FullCrudApiController):
@@ -10,6 +12,13 @@ class ServiceController(FullCrudApiController):
     response_schema = ServiceResponse
     create_schema = ServiceCreate
     update_schema = ServiceUpdate
+
+    # nuevo
+    list_dependencies = [require_read(Service)]
+    retrieve_dependencies = [require_read(Service)]
+    create_dependencies = [require_write(Service)]
+    update_dependencies = [require_write(Service)]
+    delete_dependencies = [require_delete(Service)]
 
 
 service_router = ServiceController().router
